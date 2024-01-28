c91f5e68ecc075f0f3c9ff8ab57a7ac560ff2834
Marko Arsov <mar@mca.dev>
2024-01-28 20:46:47 +0100

    Finished lab 3

---
+++ Lab3/lab3/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:lab3/screens/auth_screen.dart';
+import 'package:lab3/screens/exam_list_screen.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const ExamListScreen(),
+        '/login': (context) => const AuthScreen(isLogin: true),
+        '/register': (context) => const AuthScreen(isLogin: false),
+      },
+    );
+  }
+}
